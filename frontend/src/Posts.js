cd4233b103c36514ba74ab01b9b850de46221f1c
Shreyash <shreyashkatare21@gmail.com>
2022-03-31 23:51:58 +0530

    added css to the ui

--- frontend/src/Posts.js
+++ frontend/src/Posts.js
@@ -6,16 +6,18 @@ function Posts() {
 
     const onSubmit = async(e) => {
         e.preventDefault();
-        const data = await axios.post("http://localhost:4000/posts", {title});
+        const {data} = await axios.post("http://localhost:4000/posts", {title});
         console.log(data);
         setTitle("");
     }
   return (
-    <div>
+    <div style={{padding: "10px"}}>
         <form onSubmit={onSubmit}>
+            <div>
             <label>Title</label>
-            <input value={title} onChange={(e) => setTitle(e.target.value)} />
-            <button>Submit</button>
+            <input style={{width:"300px", height:"30px", padding:"5px", marginLeft:"10px", marginBottom:"10px", fontSize:"16px"}} value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <button style={{marginLeft:"100px"}}>Submit</button>
         </form>
     </div>
   )
